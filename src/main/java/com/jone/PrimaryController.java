93a3374e53ba54ace1fd9f8ef15cc6935535d03e
Jone <jone.chong99@gmail.com>
2023-11-15 21:18:55 +0800

    feat: implemented directory selection function

--- src/main/java/com/jone/PrimaryController.java
+++ src/main/java/com/jone/PrimaryController.java
@@ -1,12 +1,28 @@
 package com.jone;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.stage.DirectoryChooser;
+import java.io.File;
 
 public class PrimaryController {
+    @FXML
+    private Label selectedDirectoryLabel;
+    private File selectedDirectory;
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private void handleSelectDirectory() {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select Download Directory");
+
+        File chosenDirectory = directoryChooser.showDialog(null);
+
+        if (chosenDirectory != null) {
+            selectedDirectory = chosenDirectory;
+            selectedDirectoryLabel.setText(selectedDirectory.getAbsolutePath());
+            this.selectedDirectoryLabel.setText(selectedDirectory.getAbsolutePath());
+        } else {
+            return;
+        }
     }
 }
